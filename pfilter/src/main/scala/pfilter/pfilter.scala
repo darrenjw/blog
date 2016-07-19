5259cef45372547d848b5b613bd79e848381c95e
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-07-19 18:53:20 +0100

    Starting again with a simpler model...

--- pfilter/src/main/scala/pfilter/pfilter.scala
+++ pfilter/src/main/scala/pfilter/pfilter.scala
@@ -9,42 +9,38 @@ object PFilter {
 
   import scala.language.higherKinds
   import scala.collection.parallel.immutable.ParVector
-  import cats.{Functor,Foldable,Eval}
+  //import cats.{Functor,Foldable,Eval}
+  //import cats.Monoid
   //import cats.implicits._
-
-  // Functor instance for ParVector (missing from cats.implicits._)
-  implicit val pvFunctor: Functor[ParVector] = new Functor[ParVector] {
-    def map[A,B](fa: ParVector[A])(f: A => B) = fa map f
-  }
-
-  // new typeclass I need...
-  trait FoldableFunctor[F[_]] extends Functor[F] with Foldable[F]
-
-  // FoldableFunctor instance for Vector
-  implicit val vFoldFun: FoldableFunctor[Vector] = new FoldableFunctor[Vector] {
-    def map[A,B](fa: Vector[A])(f: A => B) = fa map f
-    def foldLeft[A, B](fa: Vector[A], b: B)(f: (B, A) => B): B = fa.foldLeft(b)(f)
-    def foldRight[A, B](fa: Vector[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = fa.foldRight(lb)(f)
-    }
 
 
   type LogWeight = Double
+  type State = Int
+  type Obs = Float
 
-  case class Particle[T](lw: LogWeight, v: T)
-
-  case class EDist[T, C[_]: FoldableFunctor](pc: C[Particle[T]]) {
-
-    def map[S](f: T => S): EDist[S, C] = {
-      val mp = Functor[C].map(pc)(p => Particle(p.lw, f(p.v)))
-      EDist[S, C](mp)
+  // My generic collection typeclass
+  trait GenericColl[C[_]] {
+    def map[A,B](ca: C[A])(f: A => B): C[B]
+    def reduce[A](ca: C[A])(f: (A,A) => A): A
     }
 
-  }
+  // Implementation for Vector
+  implicit val vGC: GenericColl[Vector] = new GenericColl[Vector] {
+    def map[A,B](ca: Vector[A])(f: A=>B): Vector[B] = ca map f
+    def reduce[A](ca: Vector[A])(f: (A,A)=>A): A = ca reduce f
+    }
+
+  // Implementation for ParVector
+  implicit val pvGC: GenericColl[ParVector] = new GenericColl[ParVector] {
+    def map[A,B](ca: ParVector[A])(f: A=>B): ParVector[B] = ca map f
+    def reduce[A](ca: ParVector[A])(f: (A,A)=>A): A = ca reduce f
+    }
+
 
   def main(args: Array[String]): Unit = {
-    import cats.functor
+    //import cats.functor
     println("Hi")
-    val ed = EDist(Vector(Particle(0.3, 5), Particle(0.7, 10)))
+    val ed = Vector(5,10,15)
     println(ed)
     val edm = ed map (x=>x*2)
     println(edm)
