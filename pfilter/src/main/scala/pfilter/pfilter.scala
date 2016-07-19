84a1e45d67ad6b602b3b48bbdf8503924d81fb5e
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-07-19 21:25:53 +0100

    something compiles!

--- pfilter/src/main/scala/pfilter/pfilter.scala
+++ pfilter/src/main/scala/pfilter/pfilter.scala
@@ -13,37 +13,48 @@ object PFilter {
   //import cats.Monoid
   //import cats.implicits._
 
-
   type LogWeight = Double
   type State = Int
   type Obs = Float
 
   // My generic collection typeclass
   trait GenericColl[C[_]] {
-    def map[A,B](ca: C[A])(f: A => B): C[B]
-    def reduce[A](ca: C[A])(f: (A,A) => A): A
-    }
+    def map[A, B](ca: C[A])(f: A => B): C[B]
+    def reduce[A](ca: C[A])(f: (A, A) => A): A
+  }
+  // Syntax for the typeclass
+  implicit class GenericCollSyntax[A, C[A]](value: C[A]) {
+    def map[B](f: A => B)(implicit inst: GenericColl[C]): C[B] = inst.map(value)(f)
+    def reduce(f: (A, A) => A)(implicit inst: GenericColl[C]): A = inst.reduce(value)(f)
+  }
 
   // Implementation for Vector
   implicit val vGC: GenericColl[Vector] = new GenericColl[Vector] {
-    def map[A,B](ca: Vector[A])(f: A=>B): Vector[B] = ca map f
-    def reduce[A](ca: Vector[A])(f: (A,A)=>A): A = ca reduce f
-    }
+    def map[A, B](ca: Vector[A])(f: A => B): Vector[B] = ca map f
+    def reduce[A](ca: Vector[A])(f: (A, A) => A): A = ca reduce f
+  }
 
   // Implementation for ParVector
   implicit val pvGC: GenericColl[ParVector] = new GenericColl[ParVector] {
-    def map[A,B](ca: ParVector[A])(f: A=>B): ParVector[B] = ca map f
-    def reduce[A](ca: ParVector[A])(f: (A,A)=>A): A = ca reduce f
-    }
+    def map[A, B](ca: ParVector[A])(f: A => B): ParVector[B] = ca map f
+    def reduce[A](ca: ParVector[A])(f: (A, A) => A): A = ca reduce f
+  }
 
+  // test functions to see if the concept seems to work
+  def doubleIt[C[_]: GenericColl](ca: C[State]): C[State] = ca map (_ * 2)
+  def addThem[C[_]: GenericColl](ca: C[State]): State = ca reduce (_+_)
 
   def main(args: Array[String]): Unit = {
     //import cats.functor
     println("Hi")
-    val ed = Vector(5,10,15)
+    val ed = Vector(5, 10, 15)
     println(ed)
-    val edm = ed map (x=>x*2)
-    println(edm)
+    println(doubleIt(ed))
+    println(ed.par)
+    println(doubleIt(ed.par))
+    println(addThem(ed))
+    println(addThem(ed.par))
+
     println("Bye")
   }
 
