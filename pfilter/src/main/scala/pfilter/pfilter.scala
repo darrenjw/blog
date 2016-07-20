807a77b96988d9a2335f184b7c3a2619b94ab60c
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-07-20 03:09:37 +0100

    particle filter update for a generic collection of particles

--- pfilter/src/main/scala/pfilter/pfilter.scala
+++ pfilter/src/main/scala/pfilter/pfilter.scala
@@ -5,15 +5,16 @@ Top level code for pfilter blog post
 
  */
 
+package pfilter
+
 object PFilter {
 
   import scala.language.higherKinds
   import scala.collection.parallel.immutable.ParVector
-  //import cats.{Functor,Foldable,Eval}
-  //import cats.Monoid
-  //import cats.implicits._
+  import scala.collection.GenTraversable
+  import breeze.stats.distributions.Poisson
 
-  type LogWeight = Double
+  type LogLik = Double
   type State = Int
   type Obs = Float
 
@@ -21,39 +22,62 @@ object PFilter {
   trait GenericColl[C[_]] {
     def map[A, B](ca: C[A])(f: A => B): C[B]
     def reduce[A](ca: C[A])(f: (A, A) => A): A
+    def flatMap[A, B, D[B] <: GenTraversable[B]](ca: C[A])(f: A => D[B]): C[B]
+    def zip[A, B](ca: C[A])(cb: C[B]): C[(A, B)]
+    def length[A](ca: C[A]): Int
   }
   // Syntax for the typeclass
   implicit class GenericCollSyntax[A, C[A]](value: C[A]) {
     def map[B](f: A => B)(implicit inst: GenericColl[C]): C[B] = inst.map(value)(f)
     def reduce(f: (A, A) => A)(implicit inst: GenericColl[C]): A = inst.reduce(value)(f)
+    def flatMap[B, D[B] <: GenTraversable[B]](f: A => D[B])(implicit inst: GenericColl[C]): C[B] = inst.flatMap(value)(f)
+    def zip[B](cb: C[B])(implicit inst: GenericColl[C]): C[(A, B)] = inst.zip(value)(cb)
+    def length(implicit inst: GenericColl[C]): Int = inst.length(value)
   }
 
   // Implementation for Vector
   implicit val vGC: GenericColl[Vector] = new GenericColl[Vector] {
     def map[A, B](ca: Vector[A])(f: A => B): Vector[B] = ca map f
     def reduce[A](ca: Vector[A])(f: (A, A) => A): A = ca reduce f
+    def flatMap[A, B, D[B] <: GenTraversable[B]](ca: Vector[A])(f: A => D[B]): Vector[B] = ca flatMap f
+    def zip[A, B](ca: Vector[A])(cb: Vector[B]): Vector[(A, B)] = ca zip cb
+    def length[A](ca: Vector[A]) = ca.length
   }
 
   // Implementation for ParVector
   implicit val pvGC: GenericColl[ParVector] = new GenericColl[ParVector] {
     def map[A, B](ca: ParVector[A])(f: A => B): ParVector[B] = ca map f
     def reduce[A](ca: ParVector[A])(f: (A, A) => A): A = ca reduce f
+    def flatMap[A, B, D[B] <: GenTraversable[B]](ca: ParVector[A])(f: A => D[B]): ParVector[B] = ca flatMap f
+    def zip[A, B](ca: ParVector[A])(cb: ParVector[B]): ParVector[(A, B)] = ca zip cb
+    def length[A](ca: ParVector[A]) = ca.length
   }
 
-  // test functions to see if the concept seems to work
-  def doubleIt[C[_]: GenericColl](ca: C[State]): C[State] = ca map (_ * 2)
-  def addThem[C[_]: GenericColl](ca: C[State]): State = ca reduce (_+_)
+  // TODO: Implementation for Spark RDDs
 
+
+  // Single step of a bootstrap particle filter
+  def update[C[_]: GenericColl](
+    dataLik: (State, Obs) => LogLik, stepFun: State => State
+  )(x: C[State], o: Obs): (LogLik, C[State]) = {
+    val xp = x map (stepFun(_))
+    val lw = xp map (dataLik(_, o))
+    val max = lw reduce (math.max(_, _))
+    val rw = lw map (lwi => math.exp(lwi - max))
+    val srw = rw reduce (_ + _)
+    val l = rw.length
+    val z = rw zip xp
+    val rx = z flatMap (p => Vector.fill(Poisson(p._1 * l / srw).draw)(p._2))
+    (max + math.log(srw / l), rx)
+  }
+
+
+  
   def main(args: Array[String]): Unit = {
-    //import cats.functor
     println("Hi")
     val ed = Vector(5, 10, 15)
     println(ed)
-    println(doubleIt(ed))
     println(ed.par)
-    println(doubleIt(ed.par))
-    println(addThem(ed))
-    println(addThem(ed.par))
 
     println("Bye")
   }
--- pfilter/src/test/scala/pfilter-test.scala
+++ pfilter/src/test/scala/pfilter-test.scala
@@ -5,11 +5,90 @@ Test code for pfilter
 
  */
 
+package pfilter
+
+import org.scalatest._
+import org.scalatest.junit._
+import org.junit.runner.RunWith
+
+import scala.language.higherKinds
+import PFilter._
+
+@RunWith(classOf[JUnitRunner])
+class MyTestSuite extends FunSuite {
+
+  test("1+2=3") {
+    assert(1 + 2 === 3)
+  }
+
+  // test generic functions to check that the typeclass works as intended
+  def doubleIt[C[_]: GenericColl](ca: C[Int]): C[Int] = ca map (_ * 2)
+  def addThem[C[_]: GenericColl](ca: C[Int]): State = ca reduce (_ + _)
+  def repeatThem[C[_]: GenericColl](ca: C[Int]): C[Int] = ca flatMap (x => List(x, x, x))
+  def zipThem[C[_]: GenericColl](ci: C[Int], cd: C[Double]): C[(Int, Double)] = ci zip cd
+
+  test("Vector in generic function including map") {
+    val v = Vector(5, 10, 15, 20)
+    val v2 = v map (_ * 2)
+    val v3 = doubleIt(v)
+    assert(v2 === v3)
+  }
+
+  test("Vector in generic function including flatMap") {
+    val v = Vector(5, 10, 15)
+    val v2 = v flatMap (x => Array(x, x, x))
+    //println(v2)
+    val v3 = repeatThem(v)
+    assert(v2 === v3)
+  }
+
+  test("Vector in generic function including reduce") {
+    val v = Vector(5, 10, 15)
+    val s = addThem(v)
+    assert(s === 30)
+  }
+
+  test("Vector in generic zipping function") {
+    val v1 = Vector(1, 2, 3)
+    val v2 = Vector(2.0, 4.0, 6.0)
+    val v3 = v1 zip v2
+    val v4 = zipThem(v1, v2)
+    assert(v4 === v3)
+  }
+
+  test("ParVector in generic function including map") {
+    val v = Vector(5, 10, 15, 30).par
+    val v2 = v map (_ * 2)
+    //println(v2)
+    val v3 = doubleIt(v)
+    assert(v2 === v3)
+  }
+
+  test("ParVector in generic function including flatMap") {
+    val v = Vector(5, 10, 15, 10).par
+    val v2 = v flatMap (x => Vector(x, x, x))
+    //println(v2)
+    val v3 = repeatThem(v)
+    assert(v2 === v3)
+  }
+
+  test("ParVector in generic function including reduce") {
+    val v = Vector(5, 10, 15).par
+    val s = addThem(v)
+    assert(s === 30)
+  }
+
+  test("ParVector in generic zipping function") {
+    val v1 = Vector(1, 2, 3).par
+    val v2 = Vector(2.0, 4.0, 6.0).par
+    val v3 = v1 zip v2
+    //println(v3)
+    val v4 = zipThem(v1, v2)
+    assert(v4 === v3)
+  }
 
 
 
-
-
-
+}
 
 // eof
