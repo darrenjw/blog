741151e26c425999add17e8f7e7bd70350f5aa6f
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-12-21 14:02:41 +0000

    fix a bug...

--- pi-cam/camera-script.sh
+++ pi-cam/camera-script.sh
@@ -4,7 +4,9 @@
 while [ "true" != "false" ]
 do
   DATE=$(date +"%Y-%m-%d_%H:%M:%S")
-  raspistill -w 640 -o now.jpg
+  raspistill -o now.jpg
+  ffmpeg -i now.jpg -vf scale=640:-1 now-small.jpg
+  mv now-small.jpg now.jpg
   cp now.jpg ~/timelapse/tl-$DATE.jpg # time-stamped photo
   cp now.jpg /var/www/latest.jpg # copy latest image for serving via web
   # raspistill takes around 8 seconds
