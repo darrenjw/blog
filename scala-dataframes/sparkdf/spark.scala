ac3ce6071f5b20064879d778b76f497a96f093db
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-05-06 17:14:26 +0100

    update for spark 2.1.0

--- scala-dataframes/sparkdf/spark.scala
+++ scala-dataframes/sparkdf/spark.scala
@@ -3,14 +3,14 @@ spark.scala
 
 code for a "spark-shell" session
 
-/var/tmp/spark-1.4.0-bin-hadoop2.6/bin/spark-shell --packages com.databricks:spark-csv_2.10:1.2.0 --master local[4]
+spark-shell --master local[4]
 
 */
 
-val df = sqlContext.read.format("com.databricks.spark.csv").
-                         option("header", "true").
-                         option("inferSchema","true").
-                         load("../r/cars93.csv")
+val df = spark.read.
+         option("header", "true").
+         option("inferSchema","true").
+         csv("../r/cars93.csv")
 val df2=df.filter("EngineSize <= 4.0")
 val col=df2.col("Weight")*0.453592
 val df3=df2.withColumn("WeightKG",col)
