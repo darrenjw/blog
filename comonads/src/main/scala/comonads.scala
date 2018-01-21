ace9367edf938ba43895e06b9e9d6b7380728faa
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-01-21 12:25:48 +0000

    stream and image examples working, including plots

--- comonads/src/main/scala/comonads.scala
+++ comonads/src/main/scala/comonads.scala
@@ -9,46 +9,64 @@ object ComonadExamples {
   import cats._
   import cats.implicits._
 
+  // linear filter for Stream
   def linearFilterS(weights: Stream[Double])(s: Stream[Double]): Double =
     (weights, s).parMapN(_*_).sum
 
+  // a generic linear filter
+  // (with help from Fabio Labella @SystemFw on Cats gitter channel)
   def linearFilter[F[_]: Foldable, G[_]](
     weights: F[Double], s: F[Double]
   )(implicit ev: NonEmptyParallel[F, G]): Double =
     (weights, s).parMapN(_*_).fold
 
   def filterExamples: Unit = {
-    // def s = Stream.iterate(0.0)(_+1.0)
-    def s = Stream.iterate(0.5)(x => 3.0*x*(1-x))
+    // define a steam (logistic map)
+    val lam = 3.7
+    def s = Stream.iterate(0.5)(x => lam*x*(1-x))
     println(s.take(10).toList)
     println(s.coflatten.take(3).map(_.take(4).toList).toList)
-
+    // apply a filter
     println(s.coflatMap(linearFilterS(Stream(0.25,0.5,0.25))).take(5).toList)
-
-    def myFilter(s: Stream[Double]): Double = linearFilter(Stream(0.25, 0.5, 0.25),s)
+    // now use generic filter
+    def myFilter(s: Stream[Double]): Double =
+      linearFilter(Stream(0.25, 0.5, 0.25),s)
     println(s.coflatMap(myFilter).take(5).toList)
-
-    println(s.coflatMap(s => linearFilter(Stream(0.25,0.5,0.25),s)).take(5).toList)
-    println(s.coflatMap(s => linearFilter(Stream(0.25,0.5,0.25),s)).take(1000000).drop(999995).toList)
-
-
+    // using a lambda
+    println(s.coflatMap(s => linearFilter(Stream(0.25,0.5,0.25),s)).
+      take(5).toList)
+    // check stack safety
+    println(s.coflatMap(s => linearFilter(Stream(0.25,0.5,0.25),s)).
+      take(1000000).drop(999995).toList)
+    // use a List instead of a Stream
     val sl = s.take(10).toList
     println(sl)
-    println( sl.coflatMap(sl => linearFilter(List(0.25,0.5,0.25),sl)) )
+    println(sl.coflatMap(sl => linearFilter(List(0.25,0.5,0.25),sl)) )
+    // some plots in breeze...
+    val n = 500
+    import breeze.plot._
+    import breeze.linalg._
+    val fig = Figure(s"The (smoothed) logistic map (lambda=$lam)")
+    val p0 = fig.subplot(3,1,0)
+    p0 += plot(linspace(1,n,n),s.take(n))
+    p0.ylim = (0.0,1.0)
+    p0.title = s"The logistic map (lambda=$lam)"
+    val p1 = fig.subplot(3,1,1)
+    p1 += plot(linspace(1,n,n),s.coflatMap(myFilter).take(n))
+    p1.ylim = (0.0,1.0)
+    p1.title = "Smoothed by a simple linear filter"
+    val p2 = fig.subplot(3,1,2)
+    p2 += plot(linspace(1,n,n),s.coflatMap(myFilter).coflatMap(myFilter).coflatMap(myFilter).coflatMap(myFilter).coflatMap(myFilter).take(n))
+    p2.ylim = (0.0,1.0)
+    p2.title = "Smoothed with 5 applications of the linear filter"
+    fig.refresh
   }
 
 
-  /*
-  // Stream Comonad instance - need to define "extract" and "coflatMap"...
-  // ASSUMING that the Stream is INFINTE
-  implicit val streamComonad = new Comonad[Stream] {
-    def extract[A](wa: Stream[A]) = wa.head
-    def coflatMap[A,B](wa: Stream[A])(f: Stream[A] => B): Stream[B] =
-      f(wa) #:: coflatMap(wa.tail)(f)
-    def map[A,B](wa: Stream[A])(f: A => B): Stream[B] = wa map f
-  }
-   */
 
+
+  // Numerical solution of the 2d heat equation PDE
+  // Or image processing, depending on your point of view
 
   // https://jaspervdj.be/posts/2014-11-27-comonads-image-processing.html
   // I'm going to go for column-major rather than row-major
@@ -72,6 +90,8 @@ object ComonadExamples {
         val yy = i % image.h
         f(PImage(xx, yy, image))
       })))
+    // now a few methods for navigation - not part of the comonad interface
+    // using periodic boundary conditions
     def up: PImage[T] = {
       val py = y-1
       val ny = if (py >= 0) py else (py + image.h)
@@ -94,33 +114,41 @@ object ComonadExamples {
     }
   }
 
+  // Provide evidence that PImage is a Cats Comonad
+  implicit val pimageComonad = new Comonad[PImage] {
+    def extract[A](wa: PImage[A]) = wa.extract
+    def coflatMap[A,B](wa: PImage[A])(f: PImage[A] => B): PImage[B] =
+      wa.coflatMap(f)
+    def map[A,B](wa: PImage[A])(f: A => B): PImage[B] = wa.map(f)
+  }
+
+  // convert to and from Breeze matrices
   import breeze.linalg.{Vector => BDV, _}
   def BDM2I[T](m: DenseMatrix[T]): Image[T] =
     Image(m.cols, m.rows, m.data.toVector)
-
   def I2BDM(im: Image[Double]): DenseMatrix[Double] = {
     new DenseMatrix(im.h,im.w,im.data.toArray)
   }
 
+  // examples
   def imageExamples: Unit = {
-    val pi = PImage(0,0,Image(3,3,(1 to 9).map(_.toDouble).toVector))
-    println(pi)
-    def fil(pi: PImage[Double]): Double = 0.2*(pi.extract+pi.up.extract+pi.down.extract+pi.left.extract+pi.right.extract)
-    val pif = pi.coflatMap(fil)
-    println(pif)
-
+    // a filter correspoding to one step of integration of the heat equation
+    def fil(pi: PImage[Double]): Double = (2*pi.extract+pi.up.extract+pi.down.extract+pi.left.extract+pi.right.extract)/6.0
+    // simulate a noisy image
     import breeze.stats.distributions.Gaussian
-    val bdm = DenseMatrix.tabulate(500,800){case (i,j) => math.cos(0.02*math.sqrt((i*i+j*j)))} + DenseMatrix.rand(500,800)
+    val bdm = DenseMatrix.tabulate(200,250){case (i,j) => math.cos(0.1*math.sqrt((i*i+j*j))) + Gaussian(0.0,1.0).draw}
+    val pim0 = PImage(0,0,BDM2I(bdm))
+    // just checking that Cats Comonad syntax is picked up - not required
+    pim0.coflatten 
+    // use filter to generate an infinite stream of pointed images
+    def pims = Stream.iterate(pim0)(_.coflatMap(fil))
+    // render the first few images from the stream
     import breeze.plot._
-    val fig = Figure("Random matrix")
-    val p1 = fig.subplot(2,2,0)
-    p1 += image(bdm)
-    val p2 = fig.subplot(2,2,1)
-    p2 += image(I2BDM(PImage(0,0,BDM2I(bdm)).coflatMap(fil).image))
-    val p3 = fig.subplot(2,2,2)
-    p3 += image(I2BDM(PImage(0,0,BDM2I(bdm)).coflatMap(fil).coflatMap(fil).image))
-    val p4 = fig.subplot(2,2,3)
-    p4 += image(I2BDM(PImage(0,0,BDM2I(bdm)).coflatMap(fil).coflatMap(fil).coflatMap(fil).image))
+    val fig = Figure("Diffusing a noisy image")
+    pims.take(25).zipWithIndex.foreach{case (pim,i) => {
+      val p = fig.subplot(5,5,i)
+      p += image(I2BDM(pim.image))
+    }}
 
 
   }
