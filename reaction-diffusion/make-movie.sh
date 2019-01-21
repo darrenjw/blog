6a1701e14980c8c2a579181d13111db672c27be1
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-01-21 10:20:39 +0000

    script to turn frames into movies

---
+++ reaction-diffusion/make-movie.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# make-movie.sh
+
+rm -f siv-??????-s.png
+
+for name in siv-??????.png
+do
+  short="${name%.*}"
+  echo $short
+  #pngtopnm "$name" | pnmscale 20 | pnmtopng > "${short}-s.png"
+  convert "$name" -scale 600x600 -define png:color-type=2 "${short}-s.png"
+done
+
+rm -f movie.mp4
+
+#avconv -r 20 -i siv-%06d-s.png movie.mp4
+ffmpeg -f image2 -r 6 -pattern_type glob -i 'siv-*-s.png' movie.mp4
+
+
+# eof
+
+
+
