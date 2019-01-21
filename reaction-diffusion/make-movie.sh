9ba610a457970fede540f91bcbdac78a690a0653
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-01-21 19:58:16 +0000

    real frame and movie

--- reaction-diffusion/make-movie.sh
+++ reaction-diffusion/make-movie.sh
@@ -8,13 +8,13 @@ do
   short="${name%.*}"
   echo $short
   #pngtopnm "$name" | pnmscale 20 | pnmtopng > "${short}-s.png"
-  convert "$name" -scale 1024x768 -define png:color-type=2 "${short}-s.png"
+  convert "$name" -scale 600x500 -define png:color-type=2 "${short}-s.png"
 done
 
 rm -f movie.mp4
 
 #avconv -r 20 -i siv-%06d-s.png movie.mp4
-ffmpeg -f image2 -r 6 -pattern_type glob -i 'siv-*-s.png' movie.mp4
+ffmpeg -f image2 -r 10 -pattern_type glob -i 'siv-*-s.png' movie.mp4
 
 
 # eof
