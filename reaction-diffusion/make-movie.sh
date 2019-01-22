80eb179b4527504de169427670fde7c1023567bf
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-01-22 21:55:51 +0000

    add android movie generation to the movie making script

--- reaction-diffusion/make-movie.sh
+++ reaction-diffusion/make-movie.sh
@@ -16,6 +16,8 @@ rm -f movie.mp4
 #avconv -r 20 -i siv-%06d-s.png movie.mp4
 ffmpeg -f image2 -r 10 -pattern_type glob -i 'siv-*-s.png' movie.mp4
 
+# make a version that should play on Android devices...
+ffmpeg -i movie.mp4 -codec:v libx264 -profile:v main -preset slow -b:v 400k -maxrate 400k -bufsize 800k -vf scale=-1:480 -threads 0 -codec:a libfdk_aac -b:a 128k -pix_fmt yuv420p movie-a.mp4
 
 # eof
 
