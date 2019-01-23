4854e616c75a9ac6104f8387dd1851780bef8937
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-01-23 13:18:08 +0000

    movie tweaks

--- reaction-diffusion/make-movie.sh
+++ reaction-diffusion/make-movie.sh
@@ -19,6 +19,10 @@ ffmpeg -f image2 -r 10 -pattern_type glob -i 'siv-*-s.png' movie.mp4
 # make a version that should play on Android devices...
 ffmpeg -i movie.mp4 -codec:v libx264 -profile:v main -preset slow -b:v 400k -maxrate 400k -bufsize 800k -vf scale=-1:480 -threads 0 -codec:a libfdk_aac -b:a 128k -pix_fmt yuv420p movie-a.mp4
 
+# Animated GIF...
+# ffmpeg -i movie.mp4 -s 200x100 movie.gif
+
+
 # eof
 
 
