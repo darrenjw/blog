4acbc39dde561c64f88cc59718163ee3f3e16c67
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-08-09 14:16:32 +0100

    tidy

--- min-ppl2/src/main/scala/min-ppl.scala
+++ min-ppl2/src/main/scala/min-ppl.scala
@@ -1,9 +1,16 @@
+/*
+min-ppl.scala
+
+SMC-based probability monad for bootstrap particle filter inference
+
+ */
+
 object MinPpl2 {
 
   import breeze.stats.{distributions => bdist}
   import breeze.linalg.DenseVector
 
-  implicit val numParticles = 300
+  implicit val numParticles = 500
 
   case class Particle[T](v: T, lw: Double) { // value and log-weight
     def map[S](f: T => S): Particle[S] = Particle(f(v), lw)
@@ -17,7 +24,7 @@ object MinPpl2 {
     val particles: Vector[Particle[T]]
     def draw: Particle[T]
     def mapP[S](f: T => Particle[S]): Prob[S] = Empirical(particles map (_ flatMap f))
-    def map[S](f: T => S): Prob[S] = Empirical(particles map (_ map f))
+    def map[S](f: T => S): Prob[S] = this.mapP(v => Particle(f(v), 0.0))
     def flatMap[S](f: T => Prob[S]): Prob[S] = this.mapP(f(_).draw)
     def resample(implicit N: Int): Prob[T] = {
       val lw = particles map (_.lw)
@@ -28,8 +35,7 @@ object MinPpl2 {
       val newParticles = ind map (i => particles(i))
       Empirical(newParticles.toVector map (pi => Particle(pi.v, law)))
     }
-    def cond(ll: T => Double): Prob[T] =
-      Empirical(particles map (p => Particle(p.v, p.lw + ll(p.v)))) // TODO: mapP
+    def cond(ll: T => Double): Prob[T] = this.mapP(v => Particle(v, ll(v)))
     def empirical: Vector[T] = resample.particles.map(_.v)
   }
 
@@ -50,29 +56,33 @@ object MinPpl2 {
   trait Dist[T] extends Prob[T] {
     def ll(obs: T): Double
     def ll(obs: Seq[T]): Double = obs map (ll) reduce (_+_)
-    def fit(obs: Seq[T]): Prob[T] =
-      Empirical(particles map (p => Particle(p.v, p.lw + ll(obs)))) // TODO: mapP
+    def fit(obs: Seq[T]): Prob[T] = this.mapP(v => Particle(v, ll(obs)))
     def fitQ(obs: Seq[T]): Prob[T] = Empirical(Vector(Particle(obs.head, ll(obs))))
     def fit(obs: T): Prob[T] = fit(List(obs))
     def fitQ(obs: T): Prob[T] = fitQ(List(obs))
   }
 
   case class Normal(mu: Double, v: Double)(implicit N: Int) extends Dist[Double] {
-    lazy val particles = unweighted(bdist.Gaussian(mu, math.sqrt(v)).sample(N).toVector).particles
+    lazy val particles = unweighted(bdist.Gaussian(mu, math.sqrt(v)).
+      sample(N).toVector).particles
     def draw = Particle(bdist.Gaussian(mu, math.sqrt(v)).draw, 0.0)
     def ll(obs: Double) = bdist.Gaussian(mu, math.sqrt(v)).logPdf(obs)
   }
 
   case class Gamma(a: Double, b: Double)(implicit N: Int) extends Dist[Double] {
-    lazy val particles = unweighted(bdist.Gamma(a, 1.0/b).sample(N).toVector).particles
+    lazy val particles = unweighted(bdist.Gamma(a, 1.0/b).
+      sample(N).toVector).particles
     def draw = Particle(bdist.Gamma(a, 1.0/b).draw, 0.0)
     def ll(obs: Double) = bdist.Gamma(a, 1.0/b).logPdf(obs)
   }
 
   case class Poisson(mu: Double)(implicit N: Int) extends Dist[Int] {
-    lazy val particles = unweighted(bdist.Poisson(mu).sample(N).toVector).particles
+    lazy val particles = unweighted(bdist.Poisson(mu).
+      sample(N).toVector).particles
     def draw = Particle(bdist.Poisson(mu).draw, 0.0)
     def ll(obs: Int) = bdist.Poisson(mu).logProbabilityOf(obs)
   }
 
 }
+
+// eof
